b015aed8df7055c446d9bbf42bf66acd684a37b0
Paul Ferraro <paul.ferraro@redhat.com>
2021-09-29 11:52:38 -0400

    Use authentication for hotrod testsuite.

--- hotrod/src/test/java/org/wildfly/clustering/web/spring/hotrod/AbstractSmokeITCase.java
+++ hotrod/src/test/java/org/wildfly/clustering/web/spring/hotrod/AbstractSmokeITCase.java
@@ -38,13 +38,17 @@ import org.junit.rules.TestRule;
 public abstract class AbstractSmokeITCase extends org.wildfly.clustering.web.spring.AbstractSmokeITCase {
 
     private static final String INFINISPAN_SERVER_HOME = System.getProperty("infinispan.server.home");
+    private static final String INFINISPAN_DRIVER_USERNAME = "testsuite-driver-user";
+    private static final String INFINISPAN_DRIVER_PASSWORD = "testsuite-driver-password";
 
     @ClassRule
     public static final TestRule SERVERS = InfinispanServerRuleBuilder.config(INFINISPAN_SERVER_HOME + "/server/conf/infinispan.xml")
-                .property(TestSystemPropertyNames.INFINISPAN_SERVER_HOME, INFINISPAN_SERVER_HOME)
-                .runMode(ServerRunMode.FORKED)
-                .numServers(1)
-                .build();
+            .property(TestSystemPropertyNames.INFINISPAN_TEST_SERVER_DIR, INFINISPAN_SERVER_HOME)
+            .property("infinispan.client.rest.auth_username", INFINISPAN_DRIVER_USERNAME)
+            .property("infinispan.client.rest.auth_password", INFINISPAN_DRIVER_PASSWORD)
+            .runMode(ServerRunMode.FORKED)
+            .numServers(1)
+            .build();
 
     AbstractSmokeITCase() {
         super(false);
