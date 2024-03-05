9a1c9c8f27867a3c47f40300349fedf9fed2184e
Paul Ferraro <paul.ferraro@redhat.com>
2024-03-05 16:57:29 -0500

    Workaround buggy HEAD handling in Spring, which otherwise triggers ServerHttpResponse.setComplete() twice.

--- web/core/src/test/java/org/wildfly/clustering/spring/web/servlet/DispatcherServlet.java
+++ web/core/src/test/java/org/wildfly/clustering/spring/web/servlet/DispatcherServlet.java
@@ -6,6 +6,8 @@
 package org.wildfly.clustering.spring.web.servlet;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.UnaryOperator;
 
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletConfig;
@@ -16,16 +18,21 @@ import jakarta.servlet.annotation.WebServlet;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.http.server.reactive.ServletHttpHandlerAdapter;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+
+import reactor.core.publisher.Mono;
 
 /**
  * Decorates a {@link ServletHttpHandlerAdapter} servlet to workaround the inability of Arquillian to detect programmatically registered servlets.
  * @author Paul Ferraro
  */
 @WebServlet(urlPatterns = "/", asyncSupported = true, loadOnStartup = 1)
-public class DispatcherServlet implements Servlet {
+public class DispatcherServlet implements Servlet, UnaryOperator<HttpHandler> {
 
 	static final String SERVLET_PATH = "/";
 
@@ -34,7 +41,7 @@ public class DispatcherServlet implements Servlet {
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		ApplicationContext context = (ApplicationContext) config.getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
-		HttpHandler handler = WebHttpHandlerBuilder.applicationContext(context).build();
+		HttpHandler handler = WebHttpHandlerBuilder.applicationContext(context).httpHandlerDecorator(this).build();
 		this.servlet = new ServletHttpHandlerAdapter(handler);
 		this.servlet.init(config);
 	}
@@ -57,5 +64,29 @@ public class DispatcherServlet implements Servlet {
 	@Override
 	public void destroy() {
 		this.servlet.destroy();
+	}
+
+	@Override
+	public HttpHandler apply(HttpHandler handler) {
+		UnaryOperator<ServerHttpResponse> responseDecorator = new UnaryOperator<>() {
+			@Override
+			public ServerHttpResponse apply(ServerHttpResponse response) {
+				return new ServerHttpResponseDecorator(response) {
+					private final AtomicBoolean completed = new AtomicBoolean(false);
+
+					@Override
+					public Mono<Void> setComplete() {
+						return this.completed.compareAndSet(false, true) ? response.setComplete() : Mono.empty();
+					}
+				};
+			}
+		};
+		// HttpHeadResponseDecorator is buggy and triggers ServerHttpResponse.setComplete() twice.
+		return new HttpHandler() {
+			@Override
+			public Mono<Void> handle(ServerHttpRequest request, ServerHttpResponse response) {
+				return handler.handle(request, request.getMethod().matches("HEAD") ? responseDecorator.apply(response) : response);
+			}
+		};
 	}
 }
